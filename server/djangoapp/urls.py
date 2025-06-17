eee7c9ac8e54275c56eacf3da92a2dcc1fab25a1
briighter <jaylima0@gmail.com>
2025-06-16 23:02:33 -0400

    Linting run 7

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
 
     # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>',
-        view=views.get_dealer_reviews, name='dealer_details'),
+         view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
